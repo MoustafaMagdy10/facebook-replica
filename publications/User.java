87a2869f8e187b192f8d8c6543290c1c585d9d37
MoustafaMagdy10 <moustafamohamedmagdy10@gmail.com>
2023-12-15 01:05:28 +0200

    id problem solved

--- publications/User.java
+++ publications/User.java
@@ -1,29 +1,29 @@
 package publications;
+
+import java.lang.Integer;
 import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class User {
-    private  String userName;
+    private String userName;
     private static int userIdGenerator = 0;
     private final int userId;
-    private  String gender;
-    private  String email;
-    private  String password;
+    private String gender;
+    private String email;
+    private String password;
     private int birthDay;
     private int birthMonth;
     private int birthYear;
     private String phoneNumber;
 
-    private Boolean showMyFriends=true;
-
     private static ArrayList<User> userStore = new ArrayList<User>();
     private static ArrayList<Post> postStore = new ArrayList<Post>();
     private TreeSet<Integer> posts = new TreeSet<>();
-    private ArrayList<User> friends = new ArrayList<>();
-    private ArrayList<User> pending = new ArrayList<>();
+    private TreeSet<Integer> friends = new TreeSet<>();
+    private TreeSet<Integer> pending = new TreeSet<>();
 
-
-    public User(String userName, String gender, String email, String password, int birthDay, int birthMonth, int birthYear, String phoneNumber) {
+    public User(String userName, String gender, String email, String password, int birthDay, int birthMonth,
+            int birthYear, String phoneNumber) {
         this.userName = userName;
         this.gender = gender;
         this.email = email;
@@ -36,10 +36,11 @@ public class User {
         userStore.add(this);
     }
 
-    //getters
-    public int getId(){
+    // getters
+    public int getId() {
         return userId;
     }
+
     public String getUserName() {
         return userName;
     }
@@ -71,11 +72,8 @@ public class User {
     public String getPhoneNumber() {
         return phoneNumber;
     }
-    public Boolean getShowMyFriends() {
-        return showMyFriends;
-    }
 
-    //setters
+    // setters
 
     public void setUserName(String userName) {
         this.userName = userName;
@@ -109,175 +107,183 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
-    public void setShowMyFriends(Boolean showMyFriends) {
-        this.showMyFriends = showMyFriends;
-    }
+    // functionality
 
-
-    //functionality
-
-    //used to add post in program runtime
-    public void addPost(String content, boolean isPublic){
-        Post post = new Post(userId ,content, isPublic);
+    // used to add post in program runtime
+    public void addPost(String content, boolean isPublic) {
+        Post post = new Post(userId, content, isPublic);
         posts.add(post.getId());
         postStore.add(post);
     }
 
-    //Friends
+    // Friends
     private void notifyObserver(User friend) {
-       friend.update(this.userName + " has sent you a friend request");
+        friend.update(this.userName + " has sent you a friend request");
     }
+
     private void update(String message) {
         System.out.println(message);
     }
-    private User searchUser(String friendName ,Boolean inPending ){
+
+    private User getUserByName(String friendName) {
         User friend = null;
-       if(!inPending){
-
-           for (User it: userStore){
-               if (friendName.toUpperCase().equals(it.userName.toUpperCase())){
-                   friend = it;
-               }
-           }
-
-       }else{
-           for (User it: this.pending){
-               if (friendName.toUpperCase().equals(it.getUserName().toUpperCase())){
-                   friend = it;
-               }
-           }
-       }
-        if(friend != null){
+        for (User it : userStore) {
+            if (friendName.toUpperCase().equals(it.getUserName().toUpperCase())) {
+                friend = it;
+            }
+        }
+        if (friend != null) {
             return friend;
         }
         return null;
     }
-    public void sendFriendRequest(String friendName){
-        User friend = searchUser(friendName , false);
-        if(friend != null){
-            friend.pending.add(this);
+
+    public boolean sendFriendRequest(String friendName) {
+        User friend = getUserByName(friendName);
+        if (friend != null) {
+            friend.pending.add(userId);
             this.notifyObserver(friend);
+            return true;
         }
-        else
-            System.out.println("No one such name exist");
+        return false;
     }
-    public void acceptFriendRequest(String friendName){
 
+    public boolean acceptFriendRequest(String friendName) {
 
-        User friend = searchUser(friendName , true);;
-        if(friend != null){
-            this.pending.remove(friend);
-            this.friends.add(friend);
-            friend.friends.add(this);
+        User friend = getUserByName(friendName);
+        boolean isPending = pending.contains(friend.getId());
+        if (isPending && friend != null) {
+            pending.remove(friend.userId);
+            friends.add(friend.userId);
+            friend.friends.add(userId);
+            return true;
         }
-        else{
-            System.out.println("No one with such a name exists");
+        return false;
+    }
+
+    public boolean rejectFriendRequest(String friendName) {
+        User friend = getUserByName(friendName);
+        boolean isPending = pending.contains(friend.getId());
+        if (isPending && friend != null) {
+            pending.remove(friend.userId);
+            return true;
+        }
+        return false;
+    }
+
+    public User[] showFriendRequests() {
+        User arr[] = new User[pending.size()];
+        if (pending.isEmpty()) {
+            return null;
+        }
+        int i = 0;
+        for (int it : pending) {
+            arr[i++] = getUserById(it);
+        }
+        return arr;
+    }
+
+    public User[] showFriends() {
+
+        if (friends.isEmpty()) {
+            return null;
+        }
+        int i = 0;
+        User arr[] = new User[friends.size()];
+        for (int it : friends) {
+            arr[i++] = getUserById(it);
+        }
+        return arr;
+    }
+
+    public User[] showFriends(String name) {
+        User arr[] = getUserByName(name).showFriends();
+        return arr;
+    }
+
+    public User[] showMutualFriends(String friendName, String friendName2) {
+
+        User user1 = getUserByName(friendName);
+        User user2 = getUserByName(friendName2);
+
+        if (user1 == null || user2 == null) {
+
+            return null;
         }
 
-    }
-    public void rejectFriendRequest(String friendName){
-        User friend = searchUser(friendName , true);;
-        if(friend != null){
-            this.pending.remove(friend);
-        }
-        else{
-            System.out.println("No one with such a name exists");
-        }
-    }
-    public void showFriendRequests(){
-        if(this.pending.isEmpty()){
-            System.out.println("No friend request found");
-            return;
-        }
-        for (User it : this.pending  ){
-            System.out.println(it.userName);
-        }
-    }
-    public void showFriends(){
-        for (User it : this.friends  ){
-            System.out.println(it.userName);
-        }
-    }
-    public void showFriends(String name ){
-        User friend = searchUser(name ,false );
-        if(friend == null)
-            return;
-        if(friend.friends.isEmpty()){
-            System.out.println("No friends found");
-            return;
-        }
-        if(friend.getShowMyFriends()) {
-            for (User it : friend.friends) {
-                System.out.println(it.userName);
-            }
-        }
-        else{
-            System.out.println("You can't see " + friend.userName + "'s friends");
-        }
-    }
-    public void showMutualFriends(String friendName){
-        User friend = searchUser(friendName , false);
-        if(friend == null){
-            System.out.println("No such a friend exists");
-            return;
-        }
-        for (User it : this.friends){
-            for(User it2 : friend.friends){
-                if(it.userName.equals(it2.userName)){
-                    System.out.println(it.userName);
+        int cnt = 0;
+        for (int it : user1.friends) {
+            for (int it2 : user2.friends) {
+                if (it == it2) {
+                    cnt++;
                 }
             }
         }
 
+        User[] arr = new User[cnt];
+        cnt = 0;
+
+        for (int it : user1.friends) {
+            for (int it2 : user2.friends) {
+                if (it == it2) {
+                    arr[cnt++] = getUserById(it);
+                }
+            }
+        }
+
+        return arr;
+
     }
 
-    //for loading from file when starting program
-    public static void loadPost(int publisherId,String content, boolean isPublic){
-        Post post = new Post(publisherId ,content, isPublic);
+    // for loading from file when starting program
+    public static void loadPost(int publisherId, String content, boolean isPublic) {
+        Post post = new Post(publisherId, content, isPublic);
         postStore.add(post);
     }
 
-    //for returning all the posts int the system for storing them when ending the program
-    public static Post[] exportPosts(){
+    // for returning all the posts int the system for storing them when ending the
+    // program
+    public static Post[] exportPosts() {
         Post[] arr = new Post[postStore.size()];
         int cnt = 0;
-        for(Post post : postStore){
+        for (Post post : postStore) {
             arr[cnt++] = post;
         }
 
         return arr;
     }
 
-    //return array of all users in the system
-    public static User[] exportUsers(){
+    // return array of all users in the system
+    public static User[] exportUsers() {
         User[] userArr = new User[userStore.size()];
         int cnt = 0;
-        for(User user : userStore){
+        for (User user : userStore) {
             userArr[cnt++] = user;
         }
         return userArr;
     }
 
-    public static void loadUser(String userName, String gender, String email, String password, int birthDay, int birthMonth, int birthYear, String phoneNumber){
-       User user = new User(userName,gender,email,password,birthDay,birthMonth,birthYear,phoneNumber);
-       userStore.add(user);
+    public static void loadUser(String userName, String gender, String email, String password, int birthDay,
+            int birthMonth, int birthYear, String phoneNumber) {
+        User user = new User(userName, gender, email, password, birthDay, birthMonth, birthYear, phoneNumber);
+        userStore.add(user);
     }
 
-    //returns the User of the specified
-    public static User getUserById(int userId){
+    // returns the User of the specified
+    public static User getUserById(int userId) {
         return userStore.get(userId);
     }
 
-    //returns array of all posts created by the user
-    public Post[] getPosts(){
-        //creates array of Post objects with the oldest Post being first in array
+    // returns array of all posts created by the user
+    public Post[] getPosts() {
+        // creates array of Post objects with the oldest Post being first in array
         Post[] postArr = new Post[posts.size()];
-        int cnt=0;
-        for(int post : posts){
+        int cnt = 0;
+        for (int post : posts) {
             postArr[cnt++] = postStore.get(post);
         }
-        //reverses the array so that the latest reply is the first element
-        for(int i=0; i<postArr.length/2; i++){
+        // reverses the array so that the latest reply is the first element
+        for (int i = 0; i < postArr.length / 2; i++) {
             Post tmp = postArr[i];
             postArr[i] = postArr[postArr.length - i - 1];
             postArr[postArr.length - i - 1] = tmp;
